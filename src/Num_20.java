2ce972ab58b4470f31d6bbe69269dfba1b0dec30
dmitrijsuhanov713@gmail.com <dmitrijsuhanov713@gmail.com>
2025-01-02 10:42:42 +0300

    Added Num_20

---
+++ src/Num_20.java
@@ -0,0 +1,10 @@
+record Point(int x, int y){ }
+
+public class Num_20 {
+    public static void main(String[] args){
+        Point point1 = new Point(1, 2);
+        Point point2 = new Point(7, 3);
+        System.out.println(point2);
+        System.out.println(point1);
+    }
+}
